6daa676610060ca2c215f1376a46895f9ebfbc51
Kaynan-Jamelaumn <kaynanrodrigues.nt@gmail.com>
2023-08-27 02:03:32 -0300

    .

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,20 +1,20 @@
-// import Student from '../models/Student';
+import Student from '../models/Student';
 
-// class HomeController {
-//   async index(req, res) {
-//     // const newStudent = await Student.create({
-//     //   name: 'Jamelaumn Jafar Jamal',
-//     //   lastname: 'Jabiroca',
-//     //   email: 'kaynanrodrigues.nt@gmail.com',
-//     //   birthday: '2022-01-17',
-//     //   weight: 80.0,
-//     //   height: 170.5,
+class HomeController {
+  async index(req, res) {
+    const newStudent = await Student.create({
+      name: 'Jamelaumn Jafar Jamal',
+      lastname: 'Jabiroca',
+      email: 'kaynanrodrigues.nt@gmail.com',
+      birthday: '2022-01-17',
+      weight: 80.0,
+      height: 170.5,
 
 
-//   })
-//     res.json({
-//     blabla: newStudent,
-//   });
-// }
-// }
-// export default new HomeController();
+    })
+    res.json({
+      blabla: newStudent,
+    });
+  }
+}
+export default new HomeController();
